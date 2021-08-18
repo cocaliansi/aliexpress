e37524170ea70e96328e6e76ba4794e991ab508a
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-08-18 11:44:41 +0700

    Create newali.js

---
+++ redir/newali.js
@@ -0,0 +1,5 @@
+<script type='text/javascript'>
+var this_url = window.location.href;
+var new_url = this_url.replace(&quot;https://newers01.blogspot.com/&quot;,&quot;https://newali01.blogspot.com/&quot;);
+window.location = new_url;
+</script>
