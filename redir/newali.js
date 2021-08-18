c054be59cab90dc56e42c4ecac9f8d6816d6a0b3
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-08-18 22:31:00 +0700

    Delete newali.js

--- redir/newali.js
+++
@@ -1,5 +0,0 @@
-<script type='text/javascript'>
-var this_url = window.location.href;
-var new_url = this_url.replace("https://newers01.blogspot.com/","https://newali01.blogspot.com/");
-window.location = new_url;
-</script>
