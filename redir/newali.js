e48f2472c40a5274488efb363346cf6ba86c4635
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-08-18 11:51:20 +0700

    Update newali.js

--- redir/newali.js
+++ redir/newali.js
@@ -1,5 +1,5 @@
 <script type='text/javascript'>
 var this_url = window.location.href;
-var new_url = this_url.replace(&quot;https://newers01.blogspot.com/&quot;,&quot;https://newali01.blogspot.com/&quot;);
+var new_url = this_url.replace("https://newers01.blogspot.com/","https://newali01.blogspot.com/");
 window.location = new_url;
 </script>
