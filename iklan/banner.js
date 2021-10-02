df7ada8e23d68a87548d442d6b0730d2430708c3
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-10-03 00:23:14 +0700

    Create banner.js

---
+++ iklan/banner.js
@@ -0,0 +1,2 @@
+<script async="async" data-cfasync="false" src="//cheapenleaving.com/9e82d7d6dfcb2522fc8d8af078ed1e4b/invoke.js"></script>
+<div id="container-9e82d7d6dfcb2522fc8d8af078ed1e4b"></div>
