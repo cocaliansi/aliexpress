4d6be4d02099046de8a780c5f0d058e5b20a8c7e
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-11-30 16:24:37 +0700

    Update pop.js

--- iklan/pop.js
+++ iklan/pop.js
@@ -1,1 +1,8 @@
-//cheapenleaving.com/d1/7f/54/d17f54f6bd4b1191153bca2c92ff9881.js
+	atOptions = {
+		'key' : '0ddc23d8ceda4dbfa343c6db3127e589',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/0ddc23d8ceda4dbfa343c6db3127e589/invoke.js"></scr' + 'ipt>');
