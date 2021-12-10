3afd2ec6740869bd2bd3c76892d158af10190f9c
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-12-10 23:57:44 +0700

    Update banner.js

--- iklan/banner.js
+++ iklan/banner.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '053e2b3ddb635349beba3ed8a8e161f8',
+		'key' : '7643bc3b734aba90e5bcd85f26c8559f',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/053e2b3ddb635349beba3ed8a8e161f8/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/7643bc3b734aba90e5bcd85f26c8559f/invoke.js"></scr' + 'ipt>');
