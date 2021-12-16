4b82ac8faa96ff9da3490fbe15b23eb16c0431ce
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-12-16 18:52:43 +0700

    Update 300.js

--- static/300.js
+++ static/300.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : '7643bc3b734aba90e5bcd85f26c8559f',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/7643bc3b734aba90e5bcd85f26c8559f/invoke.js"></scr' + 'ipt>');
+
