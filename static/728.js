29b17ccd3096519e275d31efcf23248021ad68e4
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2021-12-16 18:50:50 +0700

    Create 728.js

---
+++ static/728.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '556fa5e024743a92cf6509be093c4cdd',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/556fa5e024743a92cf6509be093c4cdd/invoke.js"></scr' + 'ipt>');
