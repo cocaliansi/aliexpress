28eb33c5222dd1c5fa58d65d42cadda5d183b013
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2022-01-11 15:07:40 +0700

    Create top.js

---
+++ old-static/top.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'a4c5fe443ee2184d2ed9510b7c536c61',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/a4c5fe443ee2184d2ed9510b7c536c61/invoke.js"></scr' + 'ipt>');
