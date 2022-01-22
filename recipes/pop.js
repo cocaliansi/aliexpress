84c71b5701f6dbe3030af743daca423993e88411
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2022-01-22 21:57:30 +0700

    Create pop.js

---
+++ recipes/pop.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '053e2b3ddb635349beba3ed8a8e161f8',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cheapenleaving.com/053e2b3ddb635349beba3ed8a8e161f8/invoke.js"></scr' + 'ipt>');
