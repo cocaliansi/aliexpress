9d709db3a60fb567654535a13e17f2773a8a8cfe
cocaliansi <70219901+cocaliansi@users.noreply.github.com>
2024-02-20 22:27:34 +0700

    Update pop.js

--- blog/pop.js
+++ blog/pop.js
@@ -1,1 +1,8 @@
-
+atOptions = {
+		'key' : '148df4874ce607f323ef92bb99b75cab',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//swoopanomalousgardener.com/148df4874ce607f323ef92bb99b75cab/invoke.js"></scr' + 'ipt>');
